d4672a518e830525c5a3c9d01a79448626b49865
= <vince.nyanga@hotmail.com>
2018-08-27 09:43:06 +0200

    changed character limit to 280

--- app/main/forms.py
+++ app/main/forms.py
@@ -3,5 +3,5 @@ from wtforms import TextAreaField, SubmitField
 from wtforms.validators import Length, DataRequired
 
 class SentimentForm(FlaskForm):
-    text = TextAreaField('Enter text:', validators=[DataRequired(), Length(1,140, message='maximum number of characters allowed is 140')], render_kw={'placeholder': 'Enter text (up to 140 characters)'})
+    text = TextAreaField('Enter text:', validators=[DataRequired(), Length(1,280, message='maximum number of characters allowed is 280')], render_kw={'placeholder': 'Enter text (up to 280 characters)'})
     submit = SubmitField('Get sentiment')
